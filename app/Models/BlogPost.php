bdec097a8410ea1e38d5dcc4196c39ea9e8eafe7
DereckScott <48400324+HughScott2002@users.noreply.github.com>
2022-07-21 14:57:56 -0500

    BlogPost

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -4,16 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
     use HasFactory;
 
     // mass assigned
+    use SoftDeletes;
+
     protected $fillable = ['title', 'content'];
 
     public function comments()
     {
         return $this->hasMany(Comment::class);
     }
+    // public static function boot()
+    // {
+
+    //     parent::boot();
+
+    //     static::deleting(function (BlogPost $blogPost) {
+    //         $blogPost->comments()->delete();
+    //     });
+    // }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +17,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::factory()->state([
+            'name' => 'Mary Jane Parker',
+            'email' => 'maryJane@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password', [
+                'rounds' => 12,
+            ]),
+            'remember_token' => Str::random(10),
+        ])->count(1)->create();
+
+        User::factory()->count(20)->create();
+        // dd(get_class($jane), get_class($users));
+
     }
 }
