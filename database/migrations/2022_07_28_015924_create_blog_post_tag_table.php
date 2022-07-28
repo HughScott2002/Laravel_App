49bb72300a3838b7ec6cb883b55c9c26b3149c9c
DereckScott <48400324+HughScott2002@users.noreply.github.com>
2022-07-27 20:59:40 -0500

    BlogPost Tag Table

--- database/migrations/2022_07_28_015924_create_blog_post_tag_table.php
+++ database/migrations/2022_07_28_015924_create_blog_post_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagsTable extends Migration
+class CreateBlogPostTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name', 40);
+        Schema::create('blog_post_tag', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('blog_post_tag');
     }
 }
