4039fe05856325439ded18b64edc5162a2e22c8e
DereckScott <48400324+HughScott2002@users.noreply.github.com>
2022-07-27 22:55:48 -0500

    commit

--- database/migrations/2022_07_28_015924_create_blog_post_tag_table.php
+++ database/migrations/2022_07_28_015924_create_blog_post_tag_table.php
@@ -15,6 +15,13 @@ class CreateBlogPostTagTable extends Migration
     {
         Schema::create('blog_post_tag', function (Blueprint $table) {
             $table->id();
+            // $table->unsignedBigInteger('blog_post_id')->index();
+            $table->foreignId('blog_post_id')
+                ->constrained('blog_posts')
+                ->onDelete('cascade');
+            $table->foreignId('tag_id')
+                ->constrained('tags')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
